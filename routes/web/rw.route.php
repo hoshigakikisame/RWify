b08d0d6815539070812a4bdcb61505fb579cffd3
Rian <rianatgithub@gmail.com>
2024-04-04 05:16:07 +0700

    feat: new managing warga routes

--- routes/web/rw.route.php
+++ routes/web/rw.route.php
@@ -13,12 +13,15 @@ Route::group([
 ], function() {
     Route::get('dashboard', [RWController::class, 'dashboard'])->name('dashboard');
     
+    // rw manage routes
     Route::group([
         'prefix' => 'manage',
         'as' => 'manage.'
     ], function() {
         Route::get('warga', [ManageWargaController::class, 'manageWargaPage'])->name('warga');
+        Route::post('warga/new', [ManageWargaController::class, 'addNewWarga'])->name('warga.new');
+        Route::post('warga/update', [ManageWargaController::class, 'updateWarga'])->name('warga.update');
+        Route::post('warga/delete', [ManageWargaController::class, 'deleteWarga'])->name('warga.delete');
     });
 });
 
-// rw manage routes
