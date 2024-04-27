8d09734ece12f853e5e7fecefc1de7ad57431df1
Thoriq Fathurrozi <thoriqfathurrozi@gmail.com>
2024-04-27 13:47:33 +0700

    fix(rw) : edit role warga not showing

--- resources/views/rw/manage/warga-copy.blade.php
+++ resources/views/rw/manage/warga-copy.blade.php
@@ -41,7 +41,9 @@ $rukunTetangga = \App\Models\UserModel::getRukunTetanggaOption();
                 </svg>
                 <span>Import</span>
             </button>
-            <button id="addButton" @click="modalOpen = !modalOpen" class=" flex items-center justify-center text-nowrap md:w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto gap-x-2 hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600" onclick="">
+
+
+            <button id="addButton" @click="modalOpen = !modalOpen" class=" flex items-center justify-center text-nowrap md:w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto gap-x-2 hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600" onclick="request('{{route('rw.manage.warga.new')}}', '#addModal', '#addModalForm')">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
@@ -351,7 +353,7 @@ $rukunTetangga = \App\Models\UserModel::getRukunTetanggaOption();
                             <x-selectinputform title="Status Perkawinan Warga" key="status_perkawinan" :options="$statusPerkawinan" placeholder="Pilih Status Perkawinan Warga" selected="${user.status_perkawinan}" />
                             <x-selectinputform title="Golongan Darah Warga" key="golongan_darah" :options="$golonganDarah" placeholder="Pilih Golongan Darah Warga" selected="${user.golongan_darah}" />
                             <x-selectinputform title="Tipe Warga" key="tipe_warga" :options="$tipeWarga" placeholder="Pilih Tipe Warga" selected="${user.tipe_warga}"/>
-                            <x-selectinputform title="Role Warga" key="role" :options="$role" placeholder="Pilih Role Warga" />
+                            <x-selectinputform title="Role Warga" key="role" :options="$role" placeholder="Pilih Role Warga" selected="${user.role}" />
                             <x-selectinputform title="Rukun Tetangga Warga" key="id_rukun_tetangga" :options="$rukunTetangga" placeholder="Pilih Rukun Tetangga Warga" selected="${user.id_rukun_tetangga}"/>
                             <div class="flex justify-between mt-6">
                                 <p class="text-xs text-gray-200">Note: Pastikan semua sudah terisi dengan benar</p>
@@ -407,7 +409,14 @@ $rukunTetangga = \App\Models\UserModel::getRukunTetanggaOption();
                             value.forEach(element => {
                                 $(e.currentTarget).find('#' + key).siblings('#error').append(`<li>${element}</li>`)
                             });
+
+                            setTimeout(element => {
+                                $(e.currentTarget).find('#' + key).siblings('#error').fadeOut("slow", () => {
+                                    $(e.currentTarget).find('#' + key).siblings('#error').empty()
+                                })
+                            }, 8000)
                         })
+
                     }
                 })
 
@@ -425,6 +434,5 @@ $rukunTetangga = \App\Models\UserModel::getRukunTetanggaOption();
 </script>
 
 <script type="module">
-    request('{{route("rw.manage.warga.new")}}', '#addModal', '#addModalForm')
 </script>
 @endsection
