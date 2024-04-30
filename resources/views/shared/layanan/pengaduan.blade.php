29b2cd1fbf38b2cf759e9bde12c606e83e735594
Thoriq Fathurrozi <thoriqfathurrozi@gmail.com>
2024-04-30 21:41:33 +0700

    fix(layanan): pengaduan layout

--- resources/views/shared/layanan/pengaduan.blade.php
+++ resources/views/shared/layanan/pengaduan.blade.php
@@ -3,25 +3,27 @@
 
 {{-- content --}}
 @section('content')
-<section>
+<section class="mt-7 ms-10">
     <h1 class="dark:text-gray-100">Pengaduan</h1>
-    <form action="">
-        <div class="flex gap-2">
-            <div class="">
-                <x-inputform title="Nama" key="email" type="text" placeholder="Enter Nama" />
-                <x-inputform title="NIK" key="nik" type="text" placeholder="Enter Email" />
-                <x-inputform title="Judul" key="judul" type="text" placeholder="Enter Judul" />
-                <x-textareainputform title="Deskripsi Pengaduan" key="judul" type="text" placeholder="Enter Deskripsi Pengaduan" />
+    <div class="form">
+        <form action="">
+            <div class="flex gap-2">
+                <div class="">
+                    <x-inputform title="Nama" key="email" type="text" placeholder="Enter Nama" />
+                    <x-inputform title="NIK" key="nik" type="text" placeholder="Enter Email" />
+                    <x-inputform title="Judul" key="judul" type="text" placeholder="Enter Judul" />
+                    <x-textareainputform title="Deskripsi Pengaduan" key="judul" type="text" placeholder="Enter Deskripsi Pengaduan" />
+                </div>
+                <div class="">
+                    <x-inputimage id="imagePengaduan" title="Evidence" key="image" placeholder="Gambar" />
+                </div>
             </div>
-            <div class="">
-                <x-inputimage id="imagePengaduan" title="Evidence" key="image" placeholder="Gambar" />
+            <div class="flex  mt-6">
+                <button type="submit" class="px-3 py-2 text-sm tracking-wide text-white capitalize transition-colors duration-200 transform bg-blue-500 rounded-md dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:bg-blue-700 hover:bg-blue-600 focus:outline-none focus:bg-blue-500 focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+                    Add Pengaduan
+                </button>
             </div>
-        </div>
-        <div class="flex  mt-6">
-            <button type="submit" class="px-3 py-2 text-sm tracking-wide text-white capitalize transition-colors duration-200 transform bg-blue-500 rounded-md dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:bg-blue-700 hover:bg-blue-600 focus:outline-none focus:bg-blue-500 focus:ring focus:ring-blue-300 focus:ring-opacity-50">
-                Add Pengaduan
-            </button>
-        </div>
-    </form>
+        </form>
+    </div>
 </section>
 @endsection
