b4f697d2be5eeaba8d5f60a8e3bdeabea4809c94
Thoriq Fathurrozi <thoriqfathurrozi@gmail.com>
2024-04-30 21:46:03 +0700

    fix(user): profile page background

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -3,17 +3,12 @@
 @section('content')
 @php
 $userImage = request()->user()->getImageUrl() ?? Vite::asset('resources/assets/images/avatar.jpg');
-$image = Vite::asset('resources/assets/images/Semeru.png');
+$image = Vite::asset('resources/assets/images/profileImage.jpg');
 @endphp
 <div class="mt-8 mx-14">
     <div class="relative h-full">
         <div class="background-display relative">
             <img src="{{ $image }}" alt="backgroundImage" class="h-64 w-full bg-cover rounded-xl">
-            <button class="absolute -translate-y-2/4 right-5 bottom-0 bg-gray-500/50 p-2 rounded-full fill-gray-300">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="h-4 w-4">
-                    <path d="M22.853 1.148a3.626 3.626 0 0 0-5.124 0L1.465 17.412A4.97 4.97 0 0 0 0 20.947V23a1 1 0 0 0 1 1h2.053a4.97 4.97 0 0 0 3.535-1.464L22.853 6.271a3.626 3.626 0 0 0 0-5.123M5.174 21.122A3.02 3.02 0 0 1 3.053 22H2v-1.053a2.98 2.98 0 0 1 .879-2.121L15.222 6.483l2.3 2.3ZM21.438 4.857l-2.506 2.507-2.3-2.295 2.507-2.507a1.623 1.623 0 1 1 2.295 2.3Z" />
-                </svg>
-            </button>
         </div>
         <div class="user flex gap-12 relative">
             <div class="user-avatar relative left-14 -top-20">
