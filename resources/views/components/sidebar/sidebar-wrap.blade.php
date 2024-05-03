4fc310c225946c1710de0023e3bd36fe068079b2
daffayudisaa <daffayudisa09@gmail.com>
2024-05-03 19:40:04 +0700

    fix: sidebar image profile

--- resources/views/components/sidebar/sidebar-wrap.blade.php
+++ resources/views/components/sidebar/sidebar-wrap.blade.php
@@ -13,7 +13,9 @@
             <div class="user-wrap border p-2 rounded-full text-gray-800 mb-2 w-full">
                 <div class="user flex items-center lg:gap-2 ">
                     <button id="moreButton">
-                        <img class="h-10 max-w-10 rounded-full" src="{{$imageProfile}}" alt="Thoriq Fathurrozi">
+                        <div class="w-11 rounded-full bg-white dark:bg-gray-900 h-11 bg-cover bg-center" 
+                            style="background-image: url('{{ $imageProfile }}')">
+                        </div>
                     </button>
                     <div class="body lg:min-w-10 hidden lg:block w-20 relative">
                         <h5 class="overflow-data text-sm lg:text-md dark:text-gray-200  dark:bg-gray-900 bg-white text-nowrap truncate ...">{{$role}}</h5>
