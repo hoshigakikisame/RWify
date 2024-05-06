7be7b03f453715c06e59b3530240477848f0a55d
Thoriq Fathurrozi <thoriqfathurrozi@gmail.com>
2024-05-07 00:11:26 +0700

    fix(warga): sidebar dropdown when active in page

--- resources/views/layouts/sidebar/warga-sidebar.blade.php
+++ resources/views/layouts/sidebar/warga-sidebar.blade.php
@@ -15,7 +15,7 @@ $activeDropReservasi = request()->routeIs(['warga.layanan.reservasiJadwalTemu.in
 
 @endphp
 <div class="relative" x-data="{sideDropdown: false}">
-    <button class="{{$activeDropPengaduan ? $activeStyle['active']:$activeStyle['default'];}}" @click="sideDropdown = !sideDropdown">
+    <button class="{{$activeDropPengaduan ? $activeStyle['active']:$activeStyle['default'];}}" @click="sideDropdown = !sideDropdown" x-effect="document.location.pathname.split('/').includes('pengaduan') ? sideDropdown = true : sideDropdown = false">
         <svg xmlns=" http://www.w3.org/2000/svg" viewBox="0 0 512 512" xml:space="preserve" class="w-6 fill-inherit">
             <path d="M256 319.841c-35.346 0-64 28.654-64 64v128h128v-128c0-35.346-28.654-64-64-64" />
             <path d="M362.667 383.841v128H448c35.346 0 64-28.654 64-64V253.26a42.67 42.67 0 0 0-12.011-29.696l-181.29-195.99c-31.988-34.61-85.976-36.735-120.586-4.747a85 85 0 0 0-4.747 4.747L12.395 223.5A42.67 42.67 0 0 0 0 253.58v194.261c0 35.346 28.654 64 64 64h85.333v-128c.399-58.172 47.366-105.676 104.073-107.044 58.604-1.414 108.814 46.899 109.261 107.044" />
@@ -56,7 +56,7 @@ $activeDropReservasi = request()->routeIs(['warga.layanan.reservasiJadwalTemu.in
 </div>
 
 <div class="relative" x-data="{sideDropdown: false}">
-    <button class="{{$activeDropReservasi ? $activeStyle['active']:$activeStyle['default'];}}" @click="sideDropdown = !sideDropdown">
+    <button class="{{$activeDropReservasi ? $activeStyle['active']:$activeStyle['default'];}}" @click="sideDropdown = !sideDropdown" x-effect="document.location.pathname.split('/').includes('reservasi-jadwal-temu') ? sideDropdown = true : sideDropdown = false">
         <svg xmlns=" http://www.w3.org/2000/svg" viewBox="0 0 512 512" xml:space="preserve" class="w-6 fill-inherit">
             <path d="M256 319.841c-35.346 0-64 28.654-64 64v128h128v-128c0-35.346-28.654-64-64-64" />
             <path d="M362.667 383.841v128H448c35.346 0 64-28.654 64-64V253.26a42.67 42.67 0 0 0-12.011-29.696l-181.29-195.99c-31.988-34.61-85.976-36.735-120.586-4.747a85 85 0 0 0-4.747 4.747L12.395 223.5A42.67 42.67 0 0 0 0 253.58v194.261c0 35.346 28.654 64 64 64h85.333v-128c.399-58.172 47.366-105.676 104.073-107.044 58.604-1.414 108.814 46.899 109.261 107.044" />
