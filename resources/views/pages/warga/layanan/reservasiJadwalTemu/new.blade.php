85c52bd187fd3e08864b5acecdd11689fb037e95
Thoriq Fathurrozi <thoriqfathurrozi@gmail.com>
2024-05-16 00:09:35 +0700

    fix(layanan): new reservasi page

--- resources/views/pages/warga/layanan/reservasiJadwalTemu/new.blade.php
+++ resources/views/pages/warga/layanan/reservasiJadwalTemu/new.blade.php
@@ -4,16 +4,21 @@
 {{-- content --}}
 @section('content')
 
-<section class=" mt-7 ms-10 w-1/2 mb-20">
-    <h1 class="dark:text-gray-100">Pengaduan</h1>
-    <div class="form">
+<section class="mt-7 ms-10 mb-20">
+    <div class="header mb-5 border-b pb-5 ">
+        <h1 class="dark:text-gray-100 font-medium text-lg text-gray-950">Reservasi</h1>
+        <p class="mt-1 text-sm text-gray-500 dark:text-gray-400">Buat reservasi menggunakan form ini</p>
+    </div>
+    <div class="form bg-gray-50 dark:bg-gray-800/50 px-8 py-4 pb-8 rounded-lg shadow-sm w-1/2">
         <form action="{{route('warga.layanan.reservasiJadwalTemu.new')}}" method="POST" enctype="">
             @csrf
-            <div class="flex gap-2">
-                <div class="">
-                    <x-form.input-form title="Subjek" key="subjek" type="text" placeholder="Enter Judul" value="{{old('subjek')}}" />
-                    <x-form.textarea-input-form title="Pesan Reservasi" key="pesan" type="text" placeholder="Enter Pesan Reservasi" value="{{old('subjek')}}" />
-                </div>
+            <div class="form-header mt-2 mb-6">
+                <h1 class="font-medium dark:text-gray-100">Form Reservasi</h1>
+                <p class="text-xs text-gray-500 dark:text-gray-400 mt-1">Buat reservasi dengan lengkap dengan deskripsi yang jelas</p>
+            </div>
+            <div class="form-body">
+                <x-form.input-form title="Subjek" key="subjek" type="text" placeholder="Enter Judul" value="{{old('subjek')}}" />
+                <x-form.textarea-input-form title="Pesan Reservasi" key="pesan" type="text" placeholder="Enter Pesan Reservasi" value="{{old('subjek')}}" />
                 <div class="">
                     <x-form.input-form title="Tanggal Reservasi" key="jadwal_temu" type="datetime-local" placeholder="Enter date" />
                     <div class="mt-4">
@@ -28,9 +33,9 @@
                     </div>
                 </div>
             </div>
-            <div class="flex  mt-6">
+            <div class="flex mt-5 justify-end">
                 <button type="submit" class="px-3 py-2 text-sm tracking-wide text-white capitalize transition-colors duration-200 transform bg-blue-500 rounded-md dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:bg-blue-700 hover:bg-blue-600 focus:outline-none focus:bg-blue-500 focus:ring focus:ring-blue-300 focus:ring-opacity-50">
-                    Add Reservasi
+                    Add Pengaduan
                 </button>
             </div>
         </form>
