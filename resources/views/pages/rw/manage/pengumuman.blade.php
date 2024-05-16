2b5fb5992c92ad0d97176264f1ee386d2649669c
daffayudisaa <daffayudisa09@gmail.com>
2024-05-16 11:47:54 +0700

    fix(rw): route for pengumuman detail

--- resources/views/pages/rw/manage/pengumuman.blade.php
+++ resources/views/pages/rw/manage/pengumuman.blade.php
@@ -138,7 +138,7 @@
                                     </button>
                                     <div class="absolute right-8 top-0 rounded-lg border dark:border-gray-700 overflow-hidden divide-y dark:divide-gray-700" style="display: none;" x-show="actionModalOpen" @click.away="actionModalOpen = false">
                                         <div class="relative">
-                                            <a id="detailButton" class="inline-flex gap-2 px-4 pt-3 pb-2  hover:bg-gray-100 dark:hover:bg-gray-800 w-full items-center" target="_blank" href="{{ route('informasi.pengumuman.index', ['#pengumuman-' . $pengumuman->getIdPengumuman()]) }}">
+                                            <a id="detailButton" class="inline-flex gap-2 px-4 pt-3 pb-2  hover:bg-gray-100 dark:hover:bg-gray-800 w-full items-center" target="_blank" href="{{ route('informasi.pengumuman.detail', [$pengumuman->getIdPengumuman()]) }}">
                                                 <div class="icon w-3 fill-gray-500">
                                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                                                         <path d="M23.271 9.419C21.72 6.893 18.192 2.655 12 2.655S2.28 6.893.729 9.419a4.91 4.91 0 0 0 0 5.162C2.28 17.107 5.808 21.345 12 21.345s9.72-4.238 11.271-6.764a4.91 4.91 0 0 0 0-5.162m-1.705 4.115C20.234 15.7 17.219 19.345 12 19.345S3.766 15.7 2.434 13.534a2.92 2.92 0 0 1 0-3.068C3.766 8.3 6.781 4.655 12 4.655s8.234 3.641 9.566 5.811a2.92 2.92 0 0 1 0 3.068" />
