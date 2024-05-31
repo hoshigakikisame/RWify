fc0f8896cb49dc305b73b985bec75f05fba563e8
Thoriq Fathurrozi <thoriqfathurrozi@gmail.com>
2024-06-01 04:37:04 +0700

    feat(rw): pengaduan change status popup

--- resources/views/pages/shared/layanan/pengaduan/detail.blade.php
+++ resources/views/pages/shared/layanan/pengaduan/detail.blade.php
@@ -126,11 +126,64 @@ $statusStyle = [
                                 Note: Pastikan pengaduan sudah benar benar tervalidasi sebelum merubah status pengaduan
                             </p>
                         </div>
-                        <div class="mt-4 flex justify-end">
-                            <button type="submit"
+                        <div class="mt-4 flex justify-end" x-data="{ changePopUp: false }">
+                            <button type="button" @click="changePopUp = true"
                                 class="transform rounded-md bg-blue-500 px-3 py-2 text-sm capitalize tracking-wide text-white transition-colors duration-200 hover:bg-blue-600 focus:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:bg-blue-700">
                                 Save Pengaduan
                             </button>
+
+                            <div id="changePopUp" x-show="changePopUp" class="fixed inset-0 z-40 overflow-y-auto"
+                                aria-labelledby="modal-title" role="dialog" aria-modal="true">
+                                <div
+                                    class="flex items-end justify-center min-h-screen px-4 text-center md:items-center sm:block sm:p-0">
+                                    <div x-cloak @click="()=>{changePopUp = false}" x-show="changePopUp"
+                                        x-transition:enter="transition ease-out duration-300 transform"
+                                        x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100"
+                                        x-transition:leave="transition ease-in duration-200 transform"
+                                        x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0"
+                                        class="fixed inset-0 transition-opacity bg-gray-500/40 dark:bg-gray-800/40"
+                                        aria-hidden="true"></div>
+
+                                    <div x-cloak x-show="changePopUp"
+                                        x-transition:enter="transition ease-out duration-300 transform"
+                                        x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                                        x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
+                                        x-transition:leave="transition ease-in duration-200 transform"
+                                        x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
+                                        x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                                        class="inline-block w-full max-w-xl p-8 my-20 overflow-hidden text-left transition-all transform bg-white dark:bg-gray-800 rounded-lg shadow-xl 2xl:max-w-2xl">
+                                        <div class="flex items-center justify-between space-x-4">
+                                            <h1 class="text-xl font-medium text-gray-800 dark:text-gray-100">Rubah Status
+                                                Pengaduan</h1>
+
+                                            <button @click="()=>{changePopUp = false}" type="button"
+                                                class="text-gray-600 dark:text-gray-400 focus:outline-none hover:text-gray-700 dark:hover:text-gray-500">
+                                                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none"
+                                                    viewBox="0 0 24 24" stroke="currentColor">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                        d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                                </svg>
+                                            </button>
+                                        </div>
+
+                                        <p class="mt-2 text-sm text-gray-500 ">
+                                            Merubah Pengaduan dari sistem
+                                        </p>
+
+
+                                        <h1 class="text-xl text-wrap dark:text-gray-100 tracking-wide">Apakah Anda
+                                            Yakin Merubah Pengaduan Status <span
+                                                class="font-semibold text-rose-600 underline underline-offset-8">{{ $pengaduanInstance->judul }}</span>
+                                        </h1>
+                                        <div class="flex justify-end mt-6">
+                                            <button type="submit"
+                                                class="px-3 py-2 text-sm tracking-wide text-white capitalize transition-colors duration-200 transform bg-blue-500 rounded-md dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:bg-blue-700 hover:bg-blue-600 focus:outline-none focus:bg-blue-500 focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+                                                Ubah Statu Pengaduan
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </form>
                 </div>
