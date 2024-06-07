daab5b9c6e93071ca32d47b55947a38eccc9b693
Thoriq Fathurrozi <thoriqfathurrozi@gmail.com>
2024-06-08 00:27:43 +0700

    fix(UI): logo display and favicon

--- resources/views/layouts/sidebar/sidebar.blade.php
+++ resources/views/layouts/sidebar/sidebar.blade.php
@@ -7,6 +7,12 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ Vite::asset('resources/assets/favicon/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32"
+        href="{{ Vite::asset('resources/assets/favicon/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16"
+        href="{{ Vite::asset('resources/assets/favicon/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ Vite::asset('resources/assets/favicon/site.webmanifes') }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
