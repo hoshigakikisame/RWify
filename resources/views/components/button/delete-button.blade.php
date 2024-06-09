31109fe85849188ed19b596711ec298d8884710a
daffayudisaa <daffayudisa09@gmail.com>
2024-06-09 14:37:01 +0700

    refactor(ui): change delete button element to component

---
+++ resources/views/components/button/delete-button.blade.php
@@ -0,0 +1,4 @@
+<button type="submit"
+    class="transform rounded-md bg-red-600 px-3 py-2 text-sm capitalize tracking-wide text-white transition-colors duration-200 hover:bg-red-700 focus:bg-red-500 focus:outline-none focus:ring focus:ring-red-300 focus:ring-opacity-50 dark:focus:bg-red-700">
+    <h1>{{ $title }}</h1>
+</button>
