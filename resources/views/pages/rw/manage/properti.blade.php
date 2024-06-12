0f612dc00265dbf1508aa2eb9b1116a3f136c273
Thoriq Fathurrozi <thoriqfathurrozi@gmail.com>
2024-06-12 23:46:06 +0700

    fix(component): search dropdown bug in last option

--- resources/views/pages/rw/manage/properti.blade.php
+++ resources/views/pages/rw/manage/properti.blade.php
@@ -333,7 +333,7 @@
                                                 class="block text-sm capitalize text-gray-700 dark:text-gray-300">NIK
                                                 Pemilik</label>
                                     <input id="display-nik_pemilik" placeholder="nik_pemilik" type="text" value=""
-                                                class="mt-2 block w-full rounded-md border border-gray-200 bg-white px-3 py-2 text-gray-600 placeholder-gray-400 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-500 dark:bg-gray-700 dark:text-gray-300 dark:placeholder-gray-500 dark:focus:border-blue-300 dark:focus:ring-blue-200"
+                                                class="mt-2 block w-full rounded-md border border-gray-200 bg-white px-3 py-2 text-gray-600 placeholder-gray-400 focus:border-green-400 focus:outline-none focus:ring focus:ring-green-300 focus:ring-opacity-40 dark:border-gray-500 dark:bg-gray-700 dark:text-gray-300 dark:placeholder-gray-500 dark:focus:border-green-300 dark:focus:ring-green-200"
                                                 readonly />
                                     </div>
                                     <x-form.textarea-input-form title="Alamat" key="alamat" placeholder="Alamat"  value="${properti.alamat}" />
@@ -416,7 +416,7 @@
                                                 Pemilik</label>
                                             <input id="display-nik_pemilik" placeholder="nik_pemilik" type="text"
                                                 value=""
-                                                class="mt-2 block w-full rounded-md border border-gray-200 bg-white px-3 py-2 text-gray-600 placeholder-gray-400 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-500 dark:bg-gray-700 dark:text-gray-300 dark:placeholder-gray-500 dark:focus:border-blue-300 dark:focus:ring-blue-200"
+                                                class="mt-2 block w-full rounded-md border border-gray-200 bg-white px-3 py-2 text-gray-600 placeholder-gray-400 focus:border-green-400 focus:outline-none focus:ring focus:ring-green-300 focus:ring-opacity-40 dark:border-gray-500 dark:bg-gray-700 dark:text-gray-300 dark:placeholder-gray-500 dark:focus:border-green-300 dark:focus:ring-green-200"
                                                 readonly />
                                         </div>
 
