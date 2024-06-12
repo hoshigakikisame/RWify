9df23c197b46bbb48993412bb1ac44cfe18ca5df
Thoriq Fathurrozi <thoriqfathurrozi@gmail.com>
2024-06-12 20:42:33 +0700

    fix(shared): landing page responsive design

--- resources/views/components/card/card-item-message.blade.php
+++ resources/views/components/card/card-item-message.blade.php
@@ -2,20 +2,21 @@
     $image = Vite::asset('resources/assets/images/avatar.jpg');
 @endphp
 
-<div class="card-container px-3 py-4">
+<div class="card-container px-1 xl:px-3 py-4">
     <div
-        class="card-wrap flex flex-col gap-12 overflow-hidden rounded-xl border bg-white p-10 dark:border-gray-700 dark:bg-SecondaryBg">
+        class="card-wrap flex flex-col gap-12 overflow-hidden rounded-xl border bg-white p-5 xl:p-10  dark:border-gray-700 dark:bg-SecondaryBg">
         <div class="card-header flex w-full flex-col items-center gap-4 bg-cover bg-center">
             <div class="card-avatar">
                 <img src="{{ $image }}" alt="" class="h-16 w-16 rounded-full" />
             </div>
             <div class="card-title text-center">
-                <h2 class="font-Poppins text-xl font-semibold">{{ $name }}</h2>
-                <h4 class="text-md font-Inter font-medium text-yellow-700 dark:text-[#2D9596]">{{ $position }}</h4>
+                <h2 class="font-Poppins text-lg xl:text-xl font-semibold">{{ $name }}</h2>
+                <h4 class="text-base font-Inter font-medium text-yellow-700 dark:text-[#2D9596]">{{ $position }}
+                </h4>
             </div>
         </div>
         <div class="card-body flex flex-col items-center gap-2 text-center">
-            <p class="text-sm md:text-md dark:text-gray-300">{{ $description }}</p>
+            <p class="text-sm xl:text-base dark:text-gray-300">{{ $description }}</p>
         </div>
         <div class="card-footer text-center">
             <p class="text-xs font-medium text-green-700 dark:text-gray-300">{{ $date }}</p>
